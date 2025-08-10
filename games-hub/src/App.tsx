0e6f76746944d503929d14b421ff739458f4e11a
Nauman Asghar <naumanasghar151@gmail.com>
2025-08-10 10:07:59 +0500

    initial commit

---
+++ games-hub/src/App.tsx
@@ -0,0 +1,13 @@
+// App.jsx
+import { Box, Button, Heading } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Box textAlign="center" mt={10}>
+      <Heading mb={4}>Hello Chakra UI + Vite!</Heading>
+      <Button colorScheme="teal">Click Me</Button>
+    </Box>
+  )
+}
+
+export default App
