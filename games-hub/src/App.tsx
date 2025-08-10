70d6ea759c4d97f1cbaf1b110fcf77d615046a65
Nauman Asghar <naumanasghar151@gmail.com>
2025-08-10 22:40:04 +0500

    Built a NavBar

--- games-hub/src/App.tsx
+++ games-hub/src/App.tsx
@@ -1,13 +1,34 @@
-// App.jsx
-import { Box, Button, Heading } from '@chakra-ui/react'
+
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Box textAlign="center" mt={10}>
-      <Heading mb={4}>Hello Chakra UI + Vite!</Heading>
-      <Button colorScheme="teal">Click Me</Button>
-    </Box>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav""main"`,
+        lg: `"nav nav""aside main"`,
+      }}
+      // gridTemplateColumns={{ base: "1fr", lg: "200px 1fr" }}
+    >
+      <GridItem area="nav" >
+        <Navbar></Navbar>
+      </GridItem>
+
+      
+      
+      
+          
+        <GridItem area="aside" bg="teal"  display={{ base: "none", lg: "block" }}>
+          Aside
+        </GridItem>
+      
+
+      <GridItem area="main" bg="tomato">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
